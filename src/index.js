bc76fef5d6bf25896d1fdbb52307c70f33b7a694
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-08-02 14:18:45 +0300

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    n = n.toString();
+    const digits = n.split();
+    let result = '';
+    const a = n[0];
+    const b = n[1];
+    const c = n[2];
+    if (c == 0) {
+        return result = Number(b + a);
+    } else {
+        return result = Number(c + b + a);
+    }
 }
